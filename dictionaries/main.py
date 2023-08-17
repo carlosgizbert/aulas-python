b639e61d9e95c1d8f5631c1c16aa56a5ac22b42a
Carlos Gizbert <carlos.gizbert@zapsign.com.br>
2023-08-17 10:36:11 -0300

    dictionaries2

---
+++ dictionaries/main.py
@@ -0,0 +1,26 @@
+items = {
+  "Guilherme" : 1,
+  "cachorro" : 2,
+  "nome" : 2,
+  "vindo" : 1
+}
+
+
+print('---------------')
+
+
+# percorre os items
+for elemento in items.items():
+    print(elemento) 
+
+print('---------------')
+
+# percorre os elementos
+for elemento in items:
+    print(elemento) 
+
+print('---------------')
+
+# percorre os valores
+for elemento in items.values():
+    print(elemento) 
